eca978ba27bb5fbc9201d725bb1603155c24b717
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-14 15:01:49 -0700

    Add root route and workouts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "workouts#index"
+  resources :workouts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
