fefc7ff428e975feaec8dbde40be5bd0b5310429
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-14 15:02:11 -0700

    Add workout data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+rand(10..50).times do
+  Workout.create(
+    created_at: Time.now - rand(1..364).days,
+    updated_at: Time.now - rand(1..364).days,
+    name: "Pushups",
+    reps: rand(15..50))
+end
+
+rand(10..50).times do
+  Workout.create(
+    created_at: Time.now - rand(1..364).days,
+    updated_at: Time.now - rand(1..364).days,
+    name: "Situps",
+    reps: rand(25..50))
+end
+
+rand(10..50).times do
+  Workout.create(
+    created_at: Time.now - rand(1..364).days,
+    updated_at: Time.now - rand(1..364).days,
+    name: "Squats",
+    reps: rand(15..30))
+end
+
+rand(10..50).times do
+  Workout.create(
+    created_at: Time.now - rand(1..364).days,
+    updated_at: Time.now - rand(1..364).days,
+    name: "Calf Raises",
+    reps: rand(50..100))
+end
