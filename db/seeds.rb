b9141d688d92a95eb5dd815186908e85a8b12ce7
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-14 18:54:53 -0700

    Update workout creation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,34 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-rand(10..50).times do
+rand(1..10).times do
   Workout.create(
-    created_at: Time.now - rand(1..364).days,
-    updated_at: Time.now - rand(1..364).days,
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
     name: "Pushups",
     reps: rand(15..50))
 end
 
-rand(10..50).times do
+rand(1..10).times do
   Workout.create(
-    created_at: Time.now - rand(1..364).days,
-    updated_at: Time.now - rand(1..364).days,
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
     name: "Situps",
     reps: rand(25..50))
 end
 
-rand(10..50).times do
+rand(1..10).times do
   Workout.create(
-    created_at: Time.now - rand(1..364).days,
-    updated_at: Time.now - rand(1..364).days,
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
     name: "Squats",
     reps: rand(15..30))
 end
 
-rand(10..50).times do
+rand(1..10).times do
   Workout.create(
-    created_at: Time.now - rand(1..364).days,
-    updated_at: Time.now - rand(1..364).days,
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
     name: "Calf Raises",
     reps: rand(50..100))
 end
